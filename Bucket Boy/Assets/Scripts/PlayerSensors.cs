f6c8cf516befbeb2cf166604f4720f2205e342c6
Henrik Sedvall <henrik.sedvall@gmail.com>
2021-10-12 22:24:00 +0200

    Add ability to grab objects and fix player facing direction

--- Bucket Boy/Assets/Scripts/PlayerSensors.cs
+++ Bucket Boy/Assets/Scripts/PlayerSensors.cs
@@ -5,44 +5,33 @@ using UnityEngine;
 public class PlayerSensors : MonoBehaviour
 {
     public float sightDistance = 5f; 
-    public Rigidbody2D playerBody;
-    private Vector3 oldPlayerPos;
-    private Vector3 newPlayerPos;
-    public FixedJoint2D fixedJoint2D;
     private bool isFacingLeft;
     private Vector2 facingDirection;
-    public GameObject Moveable; 
+    public Transform grabbers;
 
     void Start() 
     {
-        oldPlayerPos = this.transform.position;
-        fixedJoint2D.enabled = false;
         isFacingLeft = false;   
     }
     void Update() 
-    {
+    {   
+        PlayerSight();
         
-
-        if (Input.GetButton("Fire1"))
+        if (!Input.GetButton("Fire1"))
         {
-            PlayerSight();
-        } else {
-            //fixedJoint2D.enabled = false;
             FacingDirection();
         }
     }
 
     private void PlayerSight()
     {
-        Vector3 currentPos = this.transform.position;
+        Vector3 currentPos = grabbers.position;
         
         if (isFacingLeft == true)
         {
             facingDirection = Vector2.left;
-            currentPos.x -= 1.5f;
         } else {
             facingDirection = Vector2.right;
-            currentPos.x += 1.5f;
         }
 
         RaycastHit2D environmentInfo = Physics2D.Raycast(
@@ -50,52 +39,31 @@ public class PlayerSensors : MonoBehaviour
             facingDirection, 
             sightDistance);
 
-        if (environmentInfo.collider == true) 
+        if (environmentInfo.collider == true && environmentInfo.collider.tag == "Moveable") 
         {
-            if (environmentInfo.collider.tag == "Moveable")
+            if (Input.GetButton("Fire1"))
             {
-                Debug.Log("A MOVEABLE");
-                fixedJoint2D.enabled = true;
-                Moveable.GetComponent<FixedJoint2D>().enabled = true;
-                Moveable.GetComponent<FixedJoint2D>().connectedBody = playerBody;
-                //transform.Translate(
-                //    Vector2.right * enemySpeed * Time.deltaTime);
-            }   
+                environmentInfo.collider.gameObject.transform.parent = grabbers;
+                environmentInfo.collider.gameObject.transform.position = grabbers.position;
+                environmentInfo.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            } else {
+               environmentInfo.collider.gameObject.transform.parent = null;
+               environmentInfo.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
         }
     }
 
     private void FacingDirection()
     {
-        newPlayerPos = this.transform.position;
-        Debug.Log(newPlayerPos.x);
-        if (newPlayerPos.x < oldPlayerPos.x)
+        if (Input.GetKeyDown(KeyCode.A))
         {
             this.transform.localRotation = Quaternion.Euler(0, 180, 0);
             isFacingLeft = true;
         }
-        else if (newPlayerPos.x > oldPlayerPos.x) 
+        else if (Input.GetKeyDown(KeyCode.D)) 
         {
             this.transform.localRotation = Quaternion.Euler(0, 0, 0);
             isFacingLeft = false;
         }
-        oldPlayerPos = this.transform.position;
     }
-
-    /*void OnDrawGizmos() 
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(transform.position, Vector2.right * sightDistance);
-    }*/
-    
-    
-    /*
-    While (MouseButton Fire1 is held down)
-    {
-        Call PlayerSight
-            if collider.tag == "Moveable"
-            {
-                MovealbeMovesWithPlayer = true;
-            }
-    }
-    */ 
 }
