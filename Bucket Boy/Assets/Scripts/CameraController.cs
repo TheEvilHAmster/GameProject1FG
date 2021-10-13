928da20c712b6b31ffe8383dee73db306b193891
Henrik Sedvall <henrik.sedvall@gmail.com>
2021-10-13 19:00:13 +0200

    Clean the camera controller

--- Bucket Boy/Assets/Scripts/CameraController.cs
+++ Bucket Boy/Assets/Scripts/CameraController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     public Transform playerTransform;
     public float smoothSpeed = 3f;
     public Vector3 offset = new Vector3( 0, 0, -10);
@@ -16,34 +15,4 @@ public class CameraController : MonoBehaviour
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
     }
-    
-    /*[SerializeField] private Transform playerTransform;
-    private Camera _camera;
-    [SerializeField] private float maxHiegtOfCamera = 411;
-    [SerializeField] private float minHeightOfCamera = 0;
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if (playerTransform.position.y > minHeightOfCamera)
-        {
-            transform.position = new Vector3(transform.position.x, playerTransform.position.y, -10f); 
-        }
-        else if (playerTransform.position.y < minHeightOfCamera)
-        {
-            transform.position = new Vector3(transform.position.x, minHeightOfCamera, -10f);
-        }
-        if (playerTransform.position.y  > maxHiegtOfCamera)
-        {
-            transform.position = new Vector3(transform.position.x, maxHiegtOfCamera, -10);
-        }
-
-        
-    }
-
-    private void Awake()
-    {
-        _camera = Camera.main;
-    }*/
 }
